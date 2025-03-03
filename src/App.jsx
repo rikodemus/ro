4d91a660b59bc45d63f3a4546cdb9d02a62db574
rikodemus <gregoriusricho8@gmail.com>
2025-03-03 16:02:12 +0700

    modified:   src/App.jsx 	modified:   src/component/Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
         <Routes>
    <Route exact path='/' element={<Home/>} />
-   {/*<Route path='/Pricing' element={<Pricing/>} />*/}
+   <Route path='/Pricing' element={<Pricing/>} />
    <Route path='/Login' element={<Login/>} />
         </Routes>
    </Router>
